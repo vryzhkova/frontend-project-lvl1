b1df50b5df815abb57fe608d03e20f625ab98640
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2021-11-22 23:47:01 +0300

    step 8: fixed brain-progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,45 +1,38 @@
 #!/usr/bin/env node
 
 import runGame from '../src/index.js';
-// import getRandomNumber from '../src/getRandomNumber.js';
+import getRandomNumber from '../src/getRandomNumber.js';
 
 const description = 'What number is missing in the progression?';
 
-// const MINCOUNT = 1;
-// const MINLENGTH = 5;
-const MAXCOUNT = 10;
+const PROGRESSION_LENGTH = 10;
 
 // Создаем рандомный массив их 10 последовательных чисел
 function getArithmeticProgression() {
-  const randomNumber = Math.floor(Math.random() * 100);
-  const randomStep = Math.floor(Math.random() * 5);
-  const result = [];
+  const startNumber = getRandomNumber(100);
+  const step = getRandomNumber(5);
+  const progression = [];
 
-  for (let i = randomNumber; result.length < MAXCOUNT; i += randomStep) {
-    result.push(i);
+  for (let i = startNumber; progression.length < PROGRESSION_LENGTH; i += step) {
+    progression.push(i);
   }
-  return result;
+  return progression;
 }
 
 // Заменяем рандомное число в массиве '..'
 function createHiddenElement(arr) {
   const num = Math.floor(Math.random() * 10);
   const i = num;
-  const answer = arr[i];
-  answer = '..';
-  return arr.join(' ');
-}
-
-// Ищем спрятанное число
-function getHiddenElement() {
-  return createHiddenElement(arr)
+  const answer = String(arr[i]);
+  arr[i] = '..';
+  const stringArr = arr.join(' ');
+  return { stringArr, answer };
 }
 
 function getBrainProgressionGameData() {
-  const question = createHiddenElement(getArithmeticProgression());
-  const correctAnswer = ;
+  const { stringArr, answer } = createHiddenElement(getArithmeticProgression());
 
-  return { question, correctAnswer };
+  return { question: stringArr, correctAnswer: answer };
 }
 
 runGame(description, getBrainProgressionGameData);
--- src/getRandomNumber.js
+++ src/getRandomNumber.js
@@ -1,3 +1,5 @@
-export default function getRandomNumber() {
-  return Math.floor(Math.random() * 10);
+export default function getRandomNumber(max = 10) {
+  // return Math.floor(Math.random() * number);
+  const num = Math.random() * (max - 1) + 1;
+  return Math.round(num);
 }
